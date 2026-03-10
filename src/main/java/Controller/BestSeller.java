07442e8e482a8f9b595f57bb4bc00a425b3b60da
GOH ZHI PIN <gohzp1227@gmail.com>
2026-03-10 15:30:43 +0800

    update

--- src/main/java/Controller/BestSeller.java
+++ src/main/java/Controller/BestSeller.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("beseSeller")
+@WebServlet("/beseSeller")
 public class BestSeller extends HttpServlet {
     BeverageServices beverageServices = new BeverageServices();
 
